ff71abf58799bd6c6a5737705212c96693b8174e
Yukiko Noda <yukiko8318@gmail.com>
2023-03-22 10:16:23 +0900

    tryout heatmap

--- stanzas/heatmap-table/App.svelte
+++ stanzas/heatmap-table/App.svelte
@@ -2,13 +2,17 @@
   import metadata from "./metadata.json";
   import { camelCase } from "lodash";
   import Fa from "svelte-fa";
-  import { faCircleChevronRight } from "@fortawesome/pro-duotone-svg-icons";
+  import {
+    faCircleChevronRight,
+    faCircleDot,
+  } from "@fortawesome/pro-duotone-svg-icons";
 
   const drugIcon = "../assets/drug.png";
   const proteinIcon = "../assets/protein.png";
   const arrowTheme = {
     secondaryOpacity: 1,
     primaryColor: "#000",
+    size: "90%",
   };
 
   const params = metadata["stanza:parameter"].map((param) => {
@@ -21,36 +25,36 @@
 
 <div class="heatmap-table">
   <ul class="column-left">
-    <li class="variants">Valiants list <span class="num">1234</span></li>
-    <li class="calc-list">
+    <li>Valiants list <span class="num">1234</span></li>
+    <li>
       <img class="drug-icon" src={drugIcon} alt="drug icon" />Mutation FEP<span
         class="num">123</span
       >
     </li>
-    <li class="calc-list">
+    <li>
       <img class="protein-icon" src={proteinIcon} alt="protein icon" />Other
       Calculation 1<span class="num">123</span>
     </li>
-    <li class="calc-list">
+    <li>
       <img class="protein-icon" src={proteinIcon} alt="protein icon" />Other
       Calculation 2<span class="num">123</span>
     </li>
-    <li class="calc-list">
+    <li>
       <img class="protein-icon" src={proteinIcon} alt="protein icon" />Other
       Calculation 3<span class="num">123</span>
     </li>
-    <li class="calc-list">
+    <li>
       <img class="protein-icon" src={proteinIcon} alt="protein icon" />Other
       Calculation 4<span class="num">123</span>
     </li>
-    <li class="calc-list">
+    <li>
       <img class="protein-icon" src={proteinIcon} alt="protein icon" />Other
       Calculation 5<span class="num">123</span>
     </li>
   </ul>
   <ul class="drugs">
     <li>Drugs</li>
-    <li class="druglist">
+    <li>
       Gefitinib<Fa
         icon={faCircleChevronRight}
         {...arrowTheme}
@@ -58,42 +62,42 @@
         secondaryColor="#fcb900"
       />
     </li>
-    <li class="druglist">
+    <li>
       Elotinib<Fa
         icon={faCircleChevronRight}
         {...arrowTheme}
         secondaryColor="#fcb900"
       />
     </li>
-    <li class="druglist">
+    <li>
       Crizotinib<Fa
         icon={faCircleChevronRight}
         {...arrowTheme}
         secondaryColor="#fcb900"
       />
     </li>
-    <li class="druglist">
+    <li>
       Alectinib<Fa
         icon={faCircleChevronRight}
         {...arrowTheme}
         secondaryColor="#fcb900"
       />
     </li>
-    <li class="druglist">
+    <li>
       Lorlatinib<Fa
         icon={faCircleChevronRight}
         {...arrowTheme}
         secondaryColor="#fcb900"
       />
     </li>
-    <li class="druglist">
+    <li>
       Pralsetinib<Fa
         icon={faCircleChevronRight}
         {...arrowTheme}
         secondaryColor="#fcb900"
       />
     </li>
-    <li class="druglist">
+    <li>
       Selpercatinib<Fa
         icon={faCircleChevronRight}
         {...arrowTheme}
@@ -101,98 +105,142 @@
       />
     </li>
   </ul>
+
+  <!-- Table -->
   <div class="table-container">
     <table>
       <thead>
-        <th class="th-variant">Variant</th>
-        <th class="th-variant">HGVS</th>
-        <th class="th-significance">MGeND</th>
-        <th class="th-significance">ClinVar</th>
-        <th class="th-calculation">Calculation</th>
-        <th class="th-heatmap">Indicator1</th>
-        <th class="th-heatmap">Indicator2</th>
-        <th class="th-heatmap">Indicator3</th>
-        <th class="th-heatmap">Indicator4</th>
-        <th class="th-heatmap">Indicator5</th>
-        <th class="th-heatmap">Indicator6</th>
+        <tr>
+          <th class="th-variant" rowspan="2">Variant</th>
+          <th class="th-hgvs" rowspan="2">HGVS</th>
+          <th class="th-disease th-group" colspan="2">Significance</th>
+          <th class="th-calculation" rowspan="2">Calculation</th>
+          <th class="th-heatmap"><span>Indicator1</span></th>
+          <th class="th-heatmap"><span>Indicator2</span></th>
+          <th class="th-heatmap"><span>Indicator3</span></th>
+          <th class="th-heatmap"><span>Indicator4</span></th>
+          <th class="th-heatmap"><span>Indicator5</span></th>
+          <th class="th-heatmap"><span>Indicator6</span></th>
+        </tr>
+        <tr>
+          <th class="th-disease">MGeND</th>
+          <th class="th-disease">ClinVar</th>
+        </tr>
       </thead>
-      <tr>
-        <td class="td-variant"
-          >I1116L<Fa
-            icon={faCircleChevronRight}
-            {...arrowTheme}
-            secondaryColor="#5fdede"
-          /></td
-        >
-        <td>ENST00000256078:8:c.35G>T <br />ENST00000256078:8:p.Gly12Cys</td>
-        <td>Pathogenic</td>
-        <td>Pathogenic</td>
-        <td class="calculation"
-          ><img
-            class="drug-icon"
-            src={drugIcon}
-            alt="drug icon"
-          />MutationFEP<Fa
-            icon={faCircleChevronRight}
-            {...arrowTheme}
-            secondaryColor="#fcb900"
-          /></td
-        >
-        <td class="cell-td"><div class="cell" /></td>
-
-        <td class="cell-td"><div class="cell" /></td>
-      </tr>
-      <tr>
-        <td class="td-variant"
-          >I116T<Fa
-            icon={faCircleChevronRight}
-            {...arrowTheme}
-            secondaryColor="#5fdede"
-          /></td
-        >
-        <td
-          >ENST00000256078:8:c.35G>C <br />
-          ENST00000256078:8:p.Gly12Arg</td
-        >
-        <td>not provided</td>
-        <td>not provided</td>
-        <td><span class="cell" /><span class="cell" /></td>
-        <td><span class="cell" /><span class="cell" /></td>
-        <td><span class="cell" /><span class="cell" /></td>
-        <td><span class="cell" /><span class="cell" /></td>
-        <td><span class="cell" /><span class="cell" /></td>
-      </tr>
-      <tr>
-        <td class="td-variant"
-          >T117N<Fa
-            icon={faCircleChevronRight}
-            {...arrowTheme}
-            secondaryColor="#5fdede"
-          /></td
-        >
-        <td
-          >ENST00000256078:8:c.35G>A<br />
-          ENST00000256078:8:p.Gly12Ser</td
-        >
-        <td>Pathogenic</td>
-        <td>Pathogenic</td>
-        <td class="calculation"
-          ><img
-            class="protein-icon"
-            src={proteinIcon}
-            alt="protein icon"
-          />Other calculation<Fa
-            icon={faCircleChevronRight}
-            {...arrowTheme}
-            secondaryColor="#fcb900"
-          /></td
-        >
-        <td><span class="cell" /><span class="cell" /></td>
-        <td><span class="cell" /><span class="cell" /></td>
-        <td><span class="cell" /><span class="cell" /></td>
-        <td><span class="cell" /><span class="cell" /></td>
-        <td><span class="cell" /><span class="cell" /></td>
-      </tr>
+      <tbody>
+        <tr>
+          <td class="td-variant">
+            <label>
+              <input
+                class="radio-button"
+                type="radio"
+                name="variantid"
+                value="I1116L"
+                checked
+              />
+              I1116L<Fa
+                icon={faCircleChevronRight}
+                {...arrowTheme}
+                secondaryColor="#5fdede"
+              /></label
+            >
+          </td>
+          <td>ENST00000256078:8:c.35G>T <br />ENST00000256078:8:p.Gly12Cys</td>
+          <td>Pathogenic</td>
+          <td>Pathogenic</td>
+          <td class="td-calculation"
+            ><span
+              ><img
+                class="drug-icon"
+                src={drugIcon}
+                alt="drug icon"
+              />MutationFEP<Fa
+                icon={faCircleChevronRight}
+                {...arrowTheme}
+                secondaryColor="#fcb900"
+              /></span
+            ></td
+          >
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+        </tr>
+        <tr>
+          <td class="td-variant">
+            <label>
+              <input
+                class="radio-button"
+                type="radio"
+                name="variantid"
+                value="I116T"
+              />
+              I116T<Fa
+                icon={faCircleChevronRight}
+                {...arrowTheme}
+                secondaryColor="#5fdede"
+              />
+            </label>
+          </td>
+          <td
+            >ENST00000256078:8:c.35G>C <br />
+            ENST00000256078:8:p.Gly12Arg</td
+          >
+          <td>not provided</td>
+          <td>not provided</td>
+          <td />
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+        </tr>
+        <tr>
+          <td class="td-variant">
+            <label>
+              <input
+                class="radio-button"
+                type="radio"
+                name="variantid"
+                value="T117N"
+              />
+              T117N<Fa
+                icon={faCircleChevronRight}
+                {...arrowTheme}
+                secondaryColor="#5fdede"
+              />
+            </label></td
+          >
+          <td
+            >ENST00000256078:8:c.35G>A<br />
+            ENST00000256078:8:p.Gly12Ser</td
+          >
+          <td>Pathogenic</td>
+          <td>Pathogenic</td>
+          <td class="td-calculation"
+            ><span
+              ><img
+                class="protein-icon"
+                src={proteinIcon}
+                alt="protein icon"
+              />Other calculation<Fa
+                icon={faCircleChevronRight}
+                {...arrowTheme}
+                secondaryColor="#fcb900"
+              /></span
+            ></td
+          >
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+          <td class="cell-td"><div class="cell" /></td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </div>
