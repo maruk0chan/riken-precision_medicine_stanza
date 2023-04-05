81abed7c02287644be35e526e28ddeb2dfa9fe13
Yukiko Noda <yukiko8318@gmail.com>
2023-04-05 13:30:23 +0900

    create hgvs-keyvalue

---
+++ stanzas/hgvs-keyvalue/App.svelte
@@ -0,0 +1,20 @@
+<div class="hgvs-keyvalue">
+  <h3 class="title">Variant HGVS Information</h3>
+  <table>
+    <thead>
+      <tr><th colspan="2">Genome</th><th>Transcript</th></tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>RefSeq</th>
+        <td>xxxx</td>
+        <td>NM_05228:3c.2369C.T</td>
+      </tr>
+      <tr>
+        <th>Ensembl</th>
+        <td>xxxx</td>
+        <td>ENST000000275493.6:c.2369C.T</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
---
+++ stanzas/hgvs-keyvalue/README.md
@@ -0,0 +1,3 @@
+# HGVS Key-Value
+
+Stanza description goes here. Edit `stanzas/hgvs-keyvalue/README.md` to update.
