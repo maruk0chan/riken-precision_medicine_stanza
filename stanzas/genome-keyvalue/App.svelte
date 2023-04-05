608145d63e2b0d26856632ea452e11cf90d0a4c8
Yukiko Noda <yukiko8318@gmail.com>
2023-04-05 11:42:02 +0900

    create genome-keyvalue

---
+++ stanzas/genome-keyvalue/App.svelte
@@ -0,0 +1,17 @@
+<div class="genome-keyvalue">
+  <h3 class="title">Variant Genome Information</h3>
+  <table>
+    <tr>
+      <th>Assembly</th>
+      <td>GRCh37</td>
+    </tr>
+    <tr>
+      <th>Chromosome</th>
+      <td>7</td>
+    </tr>
+    <tr>
+      <th>Position</th>
+      <td>55249071</td>
+    </tr>
+  </table>
+</div>
---
+++ stanzas/genome-keyvalue/README.md
@@ -0,0 +1,3 @@
+# Genome Key-Value
+
+Stanza description goes here. Edit `stanzas/genome-keyvalue/README.md` to update.
