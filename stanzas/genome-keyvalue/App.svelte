676ba0cd03a09a7c474bbec4731c9ede5a186551
Yukiko Noda <yukiko8318@gmail.com>
2023-04-06 17:37:22 +0900

    change to each

--- stanzas/genome-keyvalue/App.svelte
+++ stanzas/genome-keyvalue/App.svelte
@@ -1,17 +1,19 @@
+<script>
+  const genomeList = [
+    { key: "Assembly", value: "GRCh37" },
+    { key: "Chromosome", value: "7" },
+    { key: "Position", value: "55249071" },
+  ];
+</script>
+
 <div class="genome-keyvalue">
   <h3 class="title">Variant Genome Information</h3>
   <table>
-    <tr>
-      <th>Assembly</th>
-      <td>GRCh37</td>
-    </tr>
-    <tr>
-      <th>Chromosome</th>
-      <td>7</td>
-    </tr>
-    <tr>
-      <th>Position</th>
-      <td>55249071</td>
-    </tr>
+    {#each genomeList as { key, value }}
+      <tr>
+        <th>{key}</th>
+        <td>{value}</td>
+      </tr>
+    {/each}
   </table>
 </div>
