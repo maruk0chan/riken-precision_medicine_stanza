79204b79f99b5cb5c33ec5d872172ce9e6768cff
Yukiko Noda <yukiko8318@gmail.com>
2023-05-17 15:24:35 +0900

    add Loading to search gene

--- stanzas/search-gene/App.svelte
+++ stanzas/search-gene/App.svelte
@@ -2,59 +2,63 @@
   import Fa from "svelte-fa";
   import { faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
   export let assembly, term;
+  let promise = search(term);
 
+  const thead = ["Name", "Uniprot acc", "Calculated"];
   const drugIcon =
     "https://raw.githubusercontent.com/PENQEinc/riken-precision_medicine_stanza/main/assets/drug.png";
   const proteinIcon =
     "https://raw.githubusercontent.com/PENQEinc/riken-precision_medicine_stanza/main/assets/protein.png";
 
-  let dataset = {};
-  (async (geneName) => {
-    try {
-      const response = await fetch(
-        `https://precisionmd-db.med.kyoto-u.ac.jp/api/genes/search?assembly=${assembly}&genename=${geneName}&limit=30`
-      );
-      const json = await response.json();
-      if (!response.ok) {
-        throw new Error(json);
-      }
-      dataset = json.data;
-    } catch (error) {
-      console.error(error);
+  async function search(geneName) {
+    const res = await fetch(
+      `https://precisionmd-db.med.kyoto-u.ac.jp/api/genes/search?assembly=${assembly}&genename=${geneName}&limit=300`
+    );
+    const json = await res.json();
+    if (res.ok) {
+      return json;
+    } else {
+      throw new Error(json);
     }
-  })(term);
+  }
 </script>
 
 <div class="search-gene">
   <table class="table">
     <thead>
       <tr>
-        <th>Name</th>
-        <th>Uniprot acc</th>
-        <th>Calculated</th>
+        {#each thead as th}
+          <th>{th}</th>
+        {/each}
       </tr>
     </thead>
     <tbody>
-      {#each dataset as { genename, uniprot_acc, calculation_type }}
-        <tr>
-          <td
-            ><a
-              href={`${window.location.origin}/dev/genes/details?uniprot_acc=${uniprot_acc}&assembly=${assembly}&genename=${genename}`}
-              >{genename}<Fa
-                icon={faCircleChevronRight}
-                size="90%"
-                color="var(--gene-color)"
-              />
-            </a></td
-          >
-          <td>{uniprot_acc}</td>
-          <td
-            >{#if calculation_type === "Mutation_FEP"}
-              <img src={drugIcon} alt="drug" />
-            {/if}
-          </td>
-        </tr>
-      {/each}
+      {#await promise}
+        <tr><td>Loading...</td></tr>
+      {:then dataset}
+        {#each dataset.data as { genename, uniprot_acc, calculation_type }}
+          <tr>
+            <td
+              ><a
+                href={`${window.location.origin}/dev/genes/details?uniprot_acc=${uniprot_acc}&assembly=${assembly}&genename=${genename}`}
+                >{genename}<Fa
+                  icon={faCircleChevronRight}
+                  size="90%"
+                  color="var(--gene-color)"
+                />
+              </a></td
+            >
+            <td>{uniprot_acc}</td>
+            <td
+              >{#if calculation_type === "Mutation_FEP"}
+                <img src={drugIcon} alt="drug" />
+              {/if}
+            </td>
+          </tr>
+        {/each}
+        <!-- {:catch error}
+        <tr><td style="color: red">{error.message}</td></tr> -->
+      {/await}
     </tbody>
   </table>
 </div>
