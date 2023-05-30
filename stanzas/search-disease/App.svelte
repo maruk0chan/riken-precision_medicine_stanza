d353de528862a36f22abc291eddd3f804f24f5c8
Yukiko Noda <yukiko8318@gmail.com>
2023-05-30 20:38:23 +0900

    fix genelink

--- stanzas/search-disease/App.svelte
+++ stanzas/search-disease/App.svelte
@@ -51,7 +51,7 @@
             <td
               ><a
                 class="link-gene"
-                href={`${window.location.origin}/dev//genes/details?uniprot_acc=${uniprot_acc}&assembly=${assembly}&genename=${genename}`}
+                href={`${window.location.origin}/dev/genes/details?uniprot_acc=${uniprot_acc}&assembly=${assembly}&genename=${genename}`}
                 >{genename}<Fa
                   icon={faCircleChevronRight}
                   size="90%"
